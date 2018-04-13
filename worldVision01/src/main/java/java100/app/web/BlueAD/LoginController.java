d7969396e33eb3e00e0b08436106b927c564213d
dazzul94 <dazzul94@gmail.com>
2018-04-13 11:00:24 +0900

    index, BlueAD 세팅

--- worldVision01/src/main/java/java100/app/web/BlueAD/LoginController.java
+++ worldVision01/src/main/java/java100/app/web/BlueAD/LoginController.java
@@ -1,6 +1,5 @@
-package java100.app.web.json;
+package java100.app.web.BlueAD;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.Cookie;
@@ -8,10 +7,10 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
@@ -19,26 +18,31 @@ import java100.app.domain.Member;
 import java100.app.service.FacebookService;
 import java100.app.service.MemberService;
 
-@RestController
+@Controller
 @RequestMapping("/auth")
 @SessionAttributes("loginUser")
 public class LoginController {
     
     @Autowired MemberService memberService;
-    @Autowired FacebookService facebookService; 
+    @Autowired FacebookService facebookService;
+    
+    @RequestMapping(value="login", method=RequestMethod.GET)
+    public String form(Model model) {
+        model.addAttribute("menuVisible", false);
+        return "auth/loginform";
+    }
     
     @RequestMapping(value="login", method=RequestMethod.POST)
-    public Object login(
+    public String login(
             String email, 
             String password,
-            boolean saveEmail,
+            String saveEmail,
             HttpServletResponse response,
-            HttpSession session, /* 세션 객체가 없을 경우 미리 생성할 필요가 있다.*/
             Model model) {
         
         Member member = memberService.get(email, password);
         
-        if (saveEmail) {
+        if (saveEmail != null) {
             Cookie cookie = new Cookie("email", email);
             cookie.setMaxAge(60 * 60 * 24 * 30);
             response.addCookie(cookie);
@@ -48,37 +52,35 @@ public class LoginController {
             response.addCookie(cookie);
         }
         
-        HashMap<String,Object> result = new HashMap<>();
-        
         if (member == null) {
             model.addAttribute("loginUser", null);
-            result.put("status", "fail"); 
-        } else {
-            model.addAttribute("loginUser", member);
-            result.put("status", "success");
+            model.addAttribute("menuVisible", false);
+            return "auth/loginfail"; 
         }
-        return result;
+        
+        model.addAttribute("loginUser", member);
+        
+        return "redirect:../score/list";
     }
     
     @RequestMapping(value="facebookLogin")
-    public Object facebookLogin(
+    public String facebookLogin(
             String accessToken, 
+            HttpServletResponse response,
             HttpSession session,
             Model model) {
         
-    
         try {
             @SuppressWarnings("rawtypes")
-            Map userInfo = facebookService.me(accessToken, Map.class);
+            Map result = facebookService.me(accessToken, Map.class);
             
-            Member member = memberService.get(
-                                (String)userInfo.get("email"));
+            Member member = memberService.get((String)result.get("email"));
             
             if (member == null) { // 등록된 회원이 아니면,
                 // 페이스북에서 받은 정보로 회원을 자동 등록한다.
                 member = new Member();
-                member.setName((String)userInfo.get("name"));
-                member.setEmail((String)userInfo.get("email"));
+                member.setName((String)result.get("name"));
+                member.setEmail((String)result.get("email"));
                 member.setPassword("1111");
                 memberService.add(member);
             }
@@ -86,19 +88,16 @@ public class LoginController {
             // 회원 정보를 세션에 저장하여 자동 로그인 처리를 한다.
             model.addAttribute("loginUser", member);
             
-            HashMap<String,Object> result = new HashMap<>();
-            result.put("status", "success");
-            return result;
+            return "redirect:../score/list";
             
         } catch (Exception e) {
-            HashMap<String,Object> result = new HashMap<>();
-            result.put("status", "fail");
-            return result;
+            // 액세스 토큰이 무효하다면 예외 발생!
+            return "auth/loginfail";
         }
     }
     
     @RequestMapping("logout")
-    public Object logout(HttpSession session, SessionStatus status) {
+    public String logout(HttpSession session, SessionStatus status) {
         
         // @SessionAttributes에서 관리하는 세션 데이터를 모두 제거한다.
         status.setComplete();
@@ -106,27 +105,9 @@ public class LoginController {
         // HttpSession 객체를 무효화시킨다.
         session.invalidate();
         
-        HashMap<String,Object> result = new HashMap<>();
-        result.put("status", "success");
-        return result;
+        return "redirect:login";
     }
     
-    @RequestMapping("loginUser")
-    public Object loginUser(HttpSession session) {
-        
-        Member member = (Member)session.getAttribute("loginUser");
-        
-        HashMap<String,Object> result = new HashMap<>();
-        
-        if (member != null) {
-            result.put("status", "success");
-            result.put("member", member);
-        } else {
-            result.put("status", "fail");
-        }
-            
-        return result;
-    }
     
 }
 
--- worldVision01/src/main/java/java100/app/web/interceptor/IndexInterceptor.java
+++ worldVision01/src/main/java/java100/app/web/interceptor/IndexInterceptor.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.HandlerInterceptor;
 
-public class AuthInterceptor implements HandlerInterceptor {
+public class IndexInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(
             HttpServletRequest request, 
@@ -21,7 +21,7 @@ public class AuthInterceptor implements HandlerInterceptor {
         if (session.getAttribute("loginUser") == null) {
             response.sendRedirect(
                     request.getServletContext().getContextPath() + 
-                    "/app/auth/login");
+                    "/index/auth/login");
             return false;
             // 로그인 된 상태가 아니라면 다음 인터셉터의 실행을 모두 멈추고,
             // 즉시 로그인 폼으로 간다.
