c78dceb7231003e6df6d0448418d3593da800614
dazzul94 <dazzul94@gmail.com>
2018-04-19 12:28:09 +0900

    controller push

--- worldVision01/src/main/java/java100/app/web/BlueAD/RequestController/Bluead_Online1Controller.java
+++ worldVision01/src/main/java/java100/app/web/BlueAD/RequestController/Bluead_Online1Controller.java
@@ -2,6 +2,8 @@ package java100.app.web.BlueAD.RequestController;
 
 import java.util.HashMap;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -114,13 +116,32 @@ public class Bluead_Online1Controller {
     	bluead_memberService.update(member);
         return "redirect:list";
     }
-
+*/
     @RequestMapping("delete")
     public String delete(int no) throws Exception {
 
-    	bluead_memberService.delete(no);
+        bluead_onlineService.delete(no);
         return "redirect:list";
-    }*/
+    }
+    @RequestMapping("deleteAll")
+    public String deleteAll(HttpServletRequest request, String no) throws Exception {
+        
+        String[] splitNo = no.split(",");
+        int[] realNo = new int[splitNo.length];;
+        for (int i = 0; i < splitNo.length; i++) {
+            realNo[i] = Integer.parseInt(splitNo[i]);
+            System.out.println(realNo[i]);
+        }
+        HashMap<String,Object> numbers = new HashMap<>();
+        if (realNo != null) {
+            numbers.put("numbers", realNo);
+        }
+        bluead_onlineService.deleteAll(numbers);
+       /* System.out.println((String)request.getHeader("Referer"));
+        String decodeResult = URLDecoder.decode((String)request.getHeader("Referer"), "UTF-8");
+        System.out.println(decodeResult);*/
+        return "redirect:" +  (String)request.getHeader("Referer");
+    }
 }
 
 
